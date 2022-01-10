da11bc75931f08dea8b0283744bcceee4d42fa8f
shadabwahidullah <shadabwahidullah@gmail.com>
2022-01-11 03:20:43 +0430

    Add id for genere

--- model/genere.rb
+++ model/genere.rb
@@ -2,10 +2,11 @@ require_relative './item'
 
 class Genere < Item
     attr_accessor :name
-    attr_reader :items
+    attr_reader :items, :id
 
-    def initialize(genere, author, source, label, publish_date, name)
+    def initialize(genere, author, source, label, publish_date, id, name)
         super(genere, author, source, label, publish_date)
+        @id = id
         @name = name
         @items = []
     end
