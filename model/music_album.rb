a005f96bc5eae1afcbe76531da57150195f4de30
shadabwahidullah <shadabwahidullah@gmail.com>
2022-01-11 03:03:12 +0430

    Create music_album & Implement methods for it

---
+++ model/music_album.rb
@@ -0,0 +1,14 @@
+require_relative './item'
+
+class MusicAlbum < Item 
+    attr_accessor :on_spotify
+
+    def initialize(genre, author, source, label, publish_date, on_spotify)
+        super(genre, author, source, label, publish_date)
+        @on_spotify = on_spotify
+    end
+
+    def can_be_archived?
+        on_spotify ? super : false
+    end
+end
