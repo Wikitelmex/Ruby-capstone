71c717e867f4074ddabb6474ec0d1099c4ec5455
ahmedadel56 <ahmdadl563@gmail.com>
2022-01-11 00:01:28 +0200

    Add can_be-archived , move_to_archive methods to items class

--- model/item.rb
+++ model/item.rb
@@ -1,5 +1,6 @@
 class Item
   attr_reader :id, :archived
+
   def initialize(genre, author, source, label, publish_date)
     @id = Random.rand(1..999_999)
     @archived = false
@@ -8,5 +9,13 @@ class Item
     @source = source
     @label = label
     @publish_date = publish_date
+  end
+
+  def can_be_archived?
+    @publish_date > 10
+  end
+
+  def move_to_archive
+    @archived = true if can_be_archived? == true
   end
 end
