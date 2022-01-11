8818c58e8930b6c86d082ca47d927861bd51e605
shadabwahidullah <shadabwahidullah@gmail.com>
2022-01-11 22:29:11 +0430

    Correct genre typo in add_item method & Add reader for genre items

--- model/genre.rb
+++ model/genre.rb
@@ -1,5 +1,6 @@
 class Genre
   attr_accessor :name
+  attr_reader :items
 
   def initialize(name, id = Random.rand(1..999_999))
     @id = id
@@ -9,6 +10,6 @@ class Genre
 
   def add_item(item)
     @items.push(item)
-    item.genere = self
+    item.genre = self
   end
 end
