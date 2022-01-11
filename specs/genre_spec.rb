597213c0ff94b4bc6f246dda254b11b328eb50ea
shadabwahidullah <shadabwahidullah@gmail.com>
2022-01-11 22:29:42 +0430

    Add test for adding an item to genre

--- specs/genre_spec.rb
+++ specs/genre_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../model/genre'
+require_relative '../model/item'
 
 describe Genre do
   context 'Genre Initialization' do
@@ -10,6 +11,15 @@ describe Genre do
     it '#new with id' do
       g = Genre.new 'something', 123
       expect(g).to be_a Genre
+    end
+  end
+
+  context 'Adding an item to genre' do
+    it 'add item' do
+        g = Genre.new('someGenre')
+        i = Item.new('12/12/2020')
+        g.add_item(i)
+        expect(i.genre).to eq g
     end
   end
 end
