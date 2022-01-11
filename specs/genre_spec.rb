887b0771aaf72e5a10f3b5bfd0263ffe49941d4c
shadabwahidullah <shadabwahidullah@gmail.com>
2022-01-11 21:55:08 +0430

    Add intialization test for genre with id

--- specs/genre_spec.rb
+++ specs/genre_spec.rb
@@ -2,8 +2,13 @@ require_relative '../model/genre'
 
 describe Genre do
     context "Genre Initialization" do
-        it "#new" do
+        it "#new without id" do
             g = Genre.new ('something')
+            expect(g).to be_a Genre
+        end
+
+        it "#new with id" do
+            g = Genre.new 'something', 123
             expect(g).to be_a Genre
         end
     end
