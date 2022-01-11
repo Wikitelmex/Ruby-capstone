2302b87228a10ac2e631af19d9ce8e08b9326a47
shadabwahidullah <shadabwahidullah@gmail.com>
2022-01-11 19:33:18 +0430

    Resolve linters' errors

--- model/item.rb
+++ model/item.rb
@@ -14,7 +14,7 @@ class Item
   def move_to_archive
     @archived = true if can_be_archived? == true
   end
-  
+
   def genre=(genre)
     @genre = genre
     genre.items.push(self) unless genre.items.include?(self)
