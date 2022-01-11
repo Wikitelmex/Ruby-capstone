42f756347b8c5ff821d91127e8e40b59731fdcfc
shadabwahidullah <shadabwahidullah@gmail.com>
2022-01-11 22:11:03 +0430

    Add id and acrchived in music album's intialization method

--- model/music_album.rb
+++ model/music_album.rb
@@ -3,8 +3,8 @@ require_relative './item'
 class MusicAlbum < Item
   attr_accessor :on_spotify
 
-  def initialize(publish_date, on_spotify)
-    super(publish_date)
+  def initialize(publish_date, on_spotify, id = Random.rand(1..999_999), archived: false)
+    super(publish_date, id , archived: archived)
     @on_spotify = on_spotify
   end
 
