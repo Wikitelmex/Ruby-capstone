ace94c242b3f8e98b69600d1fb05190c54f911b8
shadabwahidullah <shadabwahidullah@gmail.com>
2022-01-13 00:27:22 +0430

    Resolve linters' errors

--- view/music_albums_view.rb
+++ view/music_albums_view.rb
@@ -1,20 +1,21 @@
 require_relative '../model/music_album'
 
 class MusicAlbumView
-    def self.read_all(music_album)
-        puts "#{music_album.publish_date} On spotify: #{music_album.on_spotify}"
-    end
+  def self.read_all(music_album)
+    puts "#{music_album.publish_date} On spotify: #{music_album.on_spotify}"
+  end
 
-    def self.create
-        puts "to create a music album enter a publish date"
-        date = gets.chomp.to_s
-        puts "is it available in spotify? [Y/N]"
-        response = gets.chomp.downcase
-        if response == 'y' 
-            spotify = true
-        elsif response == 'n'
-            spotify = false
-        end
-        MusicAlbum.new(date, spotify)
+  def self.create
+    puts 'to create a music album enter a publish date'
+    date = gets.chomp.to_s
+    puts 'is it available in spotify? [Y/N]'
+    response = gets.chomp.downcase
+    case response
+    when 'y'
+      spotify = true
+    when 'n'
+      spotify = false
     end
+    MusicAlbum.new(date, spotify)
+  end
 end
