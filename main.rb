36a1081ecdb69097a4d550f34619e9d663de103a
shadabwahidullah <shadabwahidullah@gmail.com>
2022-01-13 22:36:38 +0430

    Add preseve functionality to genre

--- main.rb
+++ main.rb
@@ -6,7 +6,9 @@ def main
   books = []
   music_albums = []
   games = []
-  genres = []
+  genres_entity = Entity.new('genres.json')
+  genres = genres_entity.load(Genre)
+
   authors_entity = Entity.new('authors.json')
   authors = authors_entity.load(Author)
   ui_app = UiApp.new(books, music_albums, games, authors, genres)
@@ -21,7 +23,7 @@ def main
 
     ui_app.do_action(option)
   end
-
+  genres_entity.save(genres)
   authors_entity.save(authors)
 end
 
--- model/genre.rb
+++ model/genre.rb
@@ -2,14 +2,26 @@ class Genre
   attr_accessor :name
   attr_reader :items, :id
 
-  def initialize(name, id = Random.rand(1..999_999))
+  def initialize(name, id = Random.rand(1..999_999), items = [])
     @id = id
     @name = name
-    @items = []
+    @items = items
   end
 
   def add_item(item)
-    @items.push(item)
+    @items.push(item) unless @items.include?(item)
     item.genre = self
+  end
+
+  def to_json(_options = {})
+    {
+      name: @name,
+      id: @id, 
+      items: @items
+    }
+  end
+
+  def self.constructor_pattern
+    %w[name id items]
   end
 end
